ac397328a546bc1e18f545729e093f58104491eb
janyxh <janyxh119@163.com>
2018-11-19 10:12:58 +0800

    首次提交

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+<script>
+import {Button} from 'element-ui'
+export default {
+  
+}
+</script>
+
